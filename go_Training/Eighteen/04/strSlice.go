35b124f2b4de31d2499b52f6969ed5cbffc7cae3
miaobrother <zihao121908@gmail.com>
2018-02-05 15:09:24 +0800

    Init StrSlice

---
+++ go_Training/Eighteen/04/strSlice.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	greeting :=[]string{
+		"zhangsan",
+		"lisi",
+		"Haha",
+		"Orange",
+		"Ohyea",
+	}
+	//for i , currentEntry := range greeting{
+	//	fmt.Println(i,currentEntry)
+	//}
+	fmt.Println(greeting)
+	//for j := 0; j < len(greeting);j ++{
+	//	fmt.Println(greeting)
+	//}
+}
