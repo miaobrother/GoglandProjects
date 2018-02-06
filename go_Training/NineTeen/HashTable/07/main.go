768d2d8bcca3ab7d3f29ad4a7c0e73327285c8af
miaobrother <zihao121908@gmail.com>
2018-02-06 14:04:05 +0800

    resquests

--- go_Training/NineTeen/HashTable/07/main.go
+++ go_Training/NineTeen/HashTable/07/main.go
@@ -1,1 +1,35 @@
 package main
+
+import (
+	"fmt"
+	"bufio"
+	"log"
+	"net/http"
+)
+
+func main() {
+	res,err := http.Get("https://www.baidu.com")
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	scanner := bufio.NewScanner(res.Body)
+
+	defer res.Body.Close()
+
+	scanner.Split(bufio.ScanWords)
+	buckets := make([]int,200)
+
+	for scanner.Scan(){
+		n := hashBucket(scanner.Text())
+
+		buckets[n] ++
+	}
+	fmt.Println(buckets[65:123])
+	//fmt.Println(len(buckets[65:123]))
+
+}
+
+func hashBucket(word string) int  {
+	return int(word[0])
+}
