bb480f86abc7d55e0f6e5c32c0b7737b8d59fbf0
miaobrother <zihao121908@gmail.com>
2018-02-07 14:20:13 +0800

    Update The Reverse

--- go_Training/TwentyOne/package_sort/04/main.go
+++ go_Training/TwentyOne/package_sort/04/main.go
@@ -9,7 +9,8 @@ func main() {
 	n := []int {9,4,7,1,3,8,34,0,-9}
 	fmt.Println(n)
 
-	sort.Sort(sort.IntSlice(n))
+	//sort.Sort(sort.IntSlice(n))
+	sort.Sort(sort.Reverse(sort.IntSlice(n)))
 
 	fmt.Println(n)
 }
