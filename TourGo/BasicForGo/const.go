d000fc6d2590ab81079a33467f597033e67781af
miaobrother <zihao121908@gmail.com>
2018-02-09 10:34:49 +0800

    const常量赋值

---
+++ TourGo/BasicForGo/const.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	//"debug/elf"
+)
+
+const Pi = 3.14
+func main() {
+	const World = "世界"
+	fmt.Println("Hello","World")
+
+	fmt.Println("happy",Pi,"Day")
+
+	const  Truth = true
+
+	fmt.Println("Go rules?",Truth,World)
+}
