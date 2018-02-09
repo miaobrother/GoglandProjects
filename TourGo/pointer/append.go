289bd7ce8a01db9eaf52872047453c829a87f93f
miaobrother <zihao121908@gmail.com>
2018-02-09 17:19:51 +0800

    Append the Slice value

---
+++ TourGo/pointer/append.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var s []int
+	printSlice1(s)
+
+	s = append(s,0)
+	printSlice1(s)
+	s = append(s,1)
+	printSlice1(s)
+
+
+	s = append(s, 2, 3, 4)
+	printSlice1(s)
+}
+
+func printSlice1(s[]int)  {
+	fmt.Printf("len=%d cap=%d %v\n",len(s),cap(s),s)
+}
