f721be110acc6bc95be8df6c21b6b93d82d6723f
miaobrother <zihao121908@gmail.com>
2018-02-13 10:22:16 +0800

    The Pointer

---
+++ github/basic/pointer.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func add(a *int) int  {
+	*a = *a +1
+	return *a
+}
+
+func main() {
+	x := 3
+	fmt.Println("x=  ", x)
+
+	x1 := add(&x)
+
+	fmt.Println("x+1 = ",x1)
+
+	fmt.Println("x = ",x)
+}
