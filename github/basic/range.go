511ca0339b68e2e3694b16afcd26802296db79c0
miaobrother <zihao121908@gmail.com>
2018-02-27 17:05:34 +0800

    Reverse the data type

--- github/basic/range.go
+++ github/basic/range.go
@@ -4,14 +4,21 @@ import (
 	"fmt"
 )
 
+//func main() {
+//	a := []int{1,2,3,4}
+//	for i,v := range a{
+//		if i == 0{
+//			a[1],a[2] = 999,999
+//			fmt.Println(a)
+//		}
+//		a[i] = v + 100
+//	}
+//	fmt.Println(a)
+//}
 func main() {
-	a := []int{1,2,3,4}
-	for i,v := range a{
-		if i == 0{
-			a[1],a[2] = 999,999
-			fmt.Println(a)
-		}
-		a[i] = v + 100
+	s := []int{1,2,3,4,5,6,7}
+	for i,j := 0,len(s) -1; i < j;i,j = i +1,j-1{
+		s[i],s[j] = s[j],s[i]
 	}
-	fmt.Println(a)
+	fmt.Println(s)
 }
