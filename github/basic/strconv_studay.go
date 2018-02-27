c4327825a5c8db9871762a38c334cef7568e7bfd
miaobrother <zihao121908@gmail.com>
2018-02-27 11:09:01 +0800

    update the strconv

--- github/basic/strconv_studay.go
+++ github/basic/strconv_studay.go
@@ -3,15 +3,29 @@ package main
 import (
 	"fmt"
 	"strconv"
-	"os"
+	//"os"
 )
 
 func main() {
-	i,err := strconv.Atoi("-42")
-	if err != nil{
-		fmt.Fprintf(os.Stderr,"err:",err)
+	//i,err := strconv.Atoi("-42")
+	//if err != nil{
+	//	fmt.Fprintf(os.Stderr,"err:",err)
+	//}
+	//s:= strconv.Itoa(i)
+	//fmt.Println(s)
+	//b,err := strconv.ParseFloat("3.1415926",32)
+	//if err != nil{
+	//	fmt.Println("The error is:",err)
+	//}else {
+	//	fmt.Println(b)
+
+	//b := []byte("bool:")
+	//b = strconv.AppendBool(b,true)
+	//fmt.Println(string(b))
+
+	v := "10"
+	if s,err := strconv.Atoi(v);err ==nil{
+		fmt.Printf("%T,%v\n",s,s)
 	}
-	s:= strconv.Itoa(i)
-	fmt.Println(s)
 
 }
