ac737cfcaa0bf8ac61a87933ce16239729767ae0
miaobrother <zihao121908@gmail.com>
2018-03-05 11:17:14 +0800

    The go in Mutex

---
+++ Go_Study/go_mutex.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync/atomic"
+	"time"
+	//"encoding/binary"
+)
+
+type read0p struct {
+	key int
+	resp chan int
+}
+
+type write0p struct {
+	key int
+	val int
+	resp chan bool
+}
+
+func main() {
+	var ops int64
+
+	reads := make(chan *read0p)
+
+	writes := make(chan *write0p)
+	//fmt.Printf("The ops %T",ops)
+
+	go func() {
+		var state = make(map[int]int)
+		for {
+			select {
+			case read := <-reads:
+				read.resp <- state[read.key]
+			case write := <-writes:
+				state[write.key] = write.val
+
+				write.resp <- true
+
+
+			}
+		}
+	}()
+
+
+	for r := 0;r <100; r ++{
+		go func() {
+			for {
+				read := &read0p{
+					key :rand.Intn(5),
+					resp :make(chan int)}
+				reads <-read
+				<-read.resp
+				atomic.AddInt64(&ops,1)
+
+			}
+		}()
+	}
+
+	for w := 0;w <10;w++{
+		go func() {
+			for {
+				write := &write0p{
+					key:  rand.Intn(5),
+					val:  rand.Intn(100),
+					resp: make(chan bool)}
+				writes <- write
+				<-write.resp
+				atomic.AddInt64(&ops, 1)
+			}
+		}()
+	}
+
+	time.Sleep(time.Second)
+
+	opsFinal := atomic.LoadInt64(&ops)
+
+	fmt.Println("ops:",opsFinal)
+}
