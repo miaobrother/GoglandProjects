2cbe2f0c8cc611d83ec83869b8ef0e6ce0c2cc6f
miaobrother <zihao121908@gmail.com>
2018-03-06 17:30:47 +0800

    Go The sorted

---
+++ go_parc/go_sort.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+
+	strs := []string{"c", "a", "b"}
+
+	sort.Strings(strs)
+
+	fmt.Println("Strings:", strs)
+
+
+	ints :=[]int{6,2,3,9,0,1}
+	sort.Ints(ints)
+	fmt.Println("Ints: ",ints)
+
+
+	s := sort.IntsAreSorted(ints)
+	fmt.Println("Sorted:",s)
+}
