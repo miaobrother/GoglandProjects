5061d18821160cde5747caf043a343283cb84bb1
miaobrother <zihao121908@gmail.com>
2018-03-07 14:01:03 +0800

    go 中的切片操作

--- go_parc/go_slice.go
+++ go_parc/go_slice.go
@@ -1,1 +1,42 @@
 package main
+
+import "fmt"
+
+func main() {
+
+	s := make([]string,3)
+
+	fmt.Println("emp:",s)
+
+	s[0] = "a"
+	s[1] = "b"
+	s[2] = "c"
+
+	fmt.Println("set:",s)
+	fmt.Println("get:",s[2])
+	fmt.Println("len:",len(s))
+
+	c := make([]string,len(s))
+
+	copy(c,s)
+
+	fmt.Println("cpy:",c)
+
+	l := s[2:]
+	fmt.Println("l is:",l)
+
+
+	t := []string{"go","h","i"}
+	fmt.Println("dcl:",t)
+
+
+	twoDD := make([][]int,3)
+	for i := 0; i <3; i ++{
+		innerLen := i +1
+		twoDD[i] = make([]int,innerLen)
+		for j := 0;j < innerLen; j ++{
+			twoDD[i][j] = i +j
+		}
+	}
+	fmt.Println("2d:",twoDD)
+}
