97488f304a54d29f6c526280249f45658dd2f598
miaobrother <zihao121908@gmail.com>
2018-03-14 22:17:37 +0800

    新增两种读取文件格式

--- src/miaobrother/file_read/main.go
+++ src/miaobrother/file_read/main.go
@@ -5,6 +5,8 @@ import (
 	"os"
 	"io"
 	"bufio"
+	"io/ioutil"
+	"compress/gzip"
 )
 
 func testFile()  {
@@ -48,8 +50,44 @@ func testBufio()  {
 		fmt.Println(line)
 	}
 }
+
+func testIoutil()  {
+	data,err := ioutil.ReadFile("/Users/zhangyalei/Desktop/nginx")
+	if err != nil{
+		fmt.Printf("read file is failed %s",err)
+		return
+	}
+	fmt.Println(string(data))
+}
+func readGzip()  {
+	file,err := os.Open("/Users/zhangyalei/Desktop/nginx.tar.gz")
+	if err != nil{
+		fmt.Printf("The file is open failed,%s",err)
+		return
+	}
+	defer file.Close()
+
+	reader ,err := gzip.NewReader(file)
+	if err != nil{
+		fmt.Printf("gzip failed,err:%s",err)
+		return
+	}
+	bufreader := bufio.NewReader(reader)
+	for{
+		line,err := bufreader.ReadString('\n')
+		if err != nil{
+			if err == io.EOF{
+				break
+			}
+			fmt.Printf("The file is open failed,%s",err)
+			return
+		}
+		fmt.Printf("%s",line)
+	}
+}
 func main() {
-	testFile()
-	testBufio()
+	//testFile()
+	//testBufio()
+	testIoutil()
 
 }
