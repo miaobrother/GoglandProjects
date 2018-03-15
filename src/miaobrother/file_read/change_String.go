1ad1fb45a0e3edf5aca70c27d400a4ba13877af4
miaobrother <zihao121908@gmail.com>
2018-03-15 09:53:25 +0800

    rune 与 byte 的区别

--- src/miaobrother/file_read/change_String.go
+++ src/miaobrother/file_read/change_String.go
@@ -15,4 +15,16 @@ func main() {
 	strToByte[0] = 'i'//[97 98 99 100 101 102]
 //[105 98 99 100 101 102]
 	fmt.Println(strToByte)
+
+
+
+	var a rune = '磊'
+	fmt.Printf(" a is :%c\n",a)
+
+	//var c rune = '你'
+	var str = "中文123"
+
+	var runeSlice []rune
+	runeSlice = []rune(str)
+	fmt.Printf("str len is %d\n",len(runeSlice))
 }
