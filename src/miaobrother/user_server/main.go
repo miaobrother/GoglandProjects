b62d97309dc5463390edf95bbe20126fa1de2009
miaobrother <zihao121908@gmail.com>
2018-04-03 15:59:17 +0800

    文件以及终端获取日志

--- src/miaobrother/user_server/main.go
+++ src/miaobrother/user_server/main.go
@@ -1,9 +1,33 @@
 package main
 
 import (
-	"miaobrother/log"
+	//"miaobrother/log"
+	"miaobrother/logger"
 	//"fmt"
+	"time"
 )
+
+
+func initLogger(name,logPath,logName string,level string)(err error)  {
+	m := make(map[string]string,8)
+	m["log_path"] = logPath
+	m["log_name"] = "user_server"
+	m["log_level"] = level
+	err = logger.InitLogger(name,m)
+	if err != nil{
+		return
+	}
+	logger.Debug("init logger success..")
+	return
+}
+
+func Run()  {
+	for{
+		logger.Debug("User server is running...")
+		time.Sleep(time.Second)
+	}
+
+}
 
 func main() {
 	/*
@@ -19,7 +43,7 @@ func main() {
 	*/
 
 	//log := log.FileLogOne("") // 这是使用的接口 打印到file中
-	log := log.NewConsoleLog("") //这是使用接口 打印到console中  很简介 逼使用单个的file以及console方便很多
-	log.LogDebug("This is a debug info ")
-	log.LogWarn("This is a warn info")
+	initLogger("console","/Users/zhangyalei","gesspsbc.log","debug")
+	Run()
+	return
 }
