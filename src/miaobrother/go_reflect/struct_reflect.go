a0f844aa006fdf5ef2487e1ce5ff575bc13b869e
miaobrother <zihao121908@gmail.com>
2018-04-09 21:24:25 +0800

    使用go的反射观察struct内部的值

---
+++ src/miaobrother/go_reflect/struct_reflect.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type Student struct {
+	Name string
+	Age int
+	Sex string
+}
+
+func main() {
+
+	var s Student
+	v := reflect.ValueOf(s)
+	t := v.Type()
+	//fmt.Println(t.Kind())
+	k := t.Kind()
+	switch k {
+	case reflect.Int64:
+		fmt.Printf("s is a int64\n")
+	case reflect.Float64:
+		fmt.Printf("s is a float64\n")
+	case reflect.Struct:
+		fmt.Printf("s is a struct\n")
+		fmt.Printf("filed num of s is %d\n",t.NumField())
+		fmt.Printf("filed num of s is %d\n",v.NumField())
+		for i:= 0; i < v.NumField();i++{
+			filed := v.Field(i)
+			fmt.Printf("The s filed is : %v value is :%v\n",filed.Type(),filed.Interface())
+		}
+	}
+}
