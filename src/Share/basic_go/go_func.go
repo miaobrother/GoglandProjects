9d2f1de578f7c6144b65e0ee81d7aa6d334603b1
miaobrother <zihao121908@gmail.com>
2018-04-11 10:36:38 +0800

    go中函数的应用

---
+++ src/Share/basic_go/go_func.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+type Circle struct {
+	radius float64
+}
+
+func main() {
+
+	var cOne Circle
+	cOne.radius = 10.0
+	fmt.Println("Area of Circle(cOne=",cOne.getArea())
+
+
+
+	fmt.Println("----------")
+	//闭包和普通函数的区别
+	tmp := []int{1,2,3}
+
+	for _,i := range tmp{
+		fmt.Println(i)
+		test(i)
+	}
+
+	fmt.Println("-----------")
+
+	for _,i := range tmp{
+		fmt.Println(i)
+		defer test(i)
+	}
+
+	fmt.Println("-------------")
+
+	for _,i :=range tmp{
+		fmt.Println(i)
+		defer func() {
+			fmt.Println(i)
+		}()
+	}
+}
+
+//普通函数
+func test(i int)  {
+	fmt.Println(i)
+}
+
+func (c Circle) getArea() float64  {
+	return 3.14 * c.radius * c.radius
+}
