ce09a1512a5273419f47e6c4026e73c4b8dee4db
miaobrother <zihao121908@gmail.com>
2018-04-11 16:32:50 +0800

    新增http的post请求功能

--- src/Share/basic_go/go_http.go
+++ src/Share/basic_go/go_http.go
@@ -5,10 +5,12 @@ import (
 	"io/ioutil"
 	"net/http"
 	//"go-ethereum/core/types"
+	"bytes"
 )
 
 func main() {
 	//go 发送http请求
+	/*
 	response,err := http.Get("http://www.baidu.com")
 
 	if err != nil{
@@ -20,4 +22,20 @@ func main() {
 
 	body,_ := ioutil.ReadAll(response.Body)
 	fmt.Println(string(body))
+	*/
+
+	//http post 请求
+	body := "{\"action\":20}"
+	res,err := http.Post("https://passport.baidu.com/v2/api/?login","application/json:charset=utf-8",bytes.NewBuffer([]byte(body)))
+	if err != nil{
+		fmt.Println("Fatal err",err)
+	}
+
+	defer res.Body.Close()
+
+	content,err := ioutil.ReadAll(res.Body)
+	if err != nil{
+		fmt.Println("Fatal err",err)
+	}
+	fmt.Println(string(content))
 }
