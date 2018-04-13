0ec1188a81d5c42def909215db01bba79d6d5350
miaobrother <zihao121908@gmail.com>
2018-04-13 14:05:12 +0800

    新增go中websocket的连接方式

---
+++ src/Share/basic_go/go_STR.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	//"strings"
+)
+
+func main() {
+	var s string
+	s = "example nihao.exe"
+	fmt.Printf("\"%s\"\n", s)
+}
---
+++ src/Share/basic_go/go_web.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/net/websocket"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func Echo(ws *websocket.Conn)  {
+	var err error
+	for{
+		var reply string
+		if err = websocket.Message.Receive(ws,&reply);err!= nil{
+			fmt.Println("can't receive")
+			break
+		}
+		fmt.Println("receive back from client:"+ reply)
+
+		msg := "receive:" + reply
+
+		fmt.Println("send to client:" + msg)
+
+		if err = websocket.Message.Send(ws,msg);err != nil{
+			break
+		}
+	}
+}
+
+func web(w http.ResponseWriter,r * http.Request)  {
+	// print method
+	fmt.Println("method is",r.Method)
+	if r.Method == "GET"{
+		tt,_ := template.ParseFiles("websocket.html")
+		tt.Execute(w,nil)
+	}else {
+		fmt.Println(r.PostFormValue("username"))
+	}
+}
+
+func main() {
+
+	//接受websocket的路由地址
+	 http.Handle("/websocket",websocket.Handler(Echo))
+
+	 http.HandleFunc("/web",web)
+
+	 if err := http.ListenAndServe(":12345",nil);err !=nil{
+	 	log.Fatal("ListenAndServer:",err)
+	 }
+}
