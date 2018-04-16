963527e8f5a8b2b28d6108339fc4a4fbf15fbb6e
miaobrother <zihao121908@gmail.com>
2018-04-16 21:28:31 +0800

    struct中还有指针类型

--- src/Share/advance_go/go_student_struct.go
+++ src/Share/advance_go/go_student_struct.go
@@ -24,4 +24,12 @@ func main() {
 	sTwo := Student{addr:"hengshui"}
 
 	fmt.Printf("The stuOne is %#v\n ",sTwo)
+
+
+
+
+	var ptr *Student = &Student{//顺序初始化 每个程序必须初始化
+		1,"lisi",'f',26,"handan",
+	}
+	fmt.Printf("The ptr is %#v\n ",*ptr)
 }
