5cdc5b210217eef22a2d7740123d8dc2452f966e
miaobrother <zihao121908@gmail.com>
2018-04-16 21:24:04 +0800

    struct中两种初始化方式 （顺序初始化以及指定初始化）

---
+++ src/Share/advance_go/go_student_struct.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	//"net/http"
+)
+
+
+type Student struct {
+	id int
+	name string
+	sex byte
+	age int
+	addr string
+}
+
+func main() {
+	var stuOne Student = Student{//顺序初始化 每个程序必须初始化
+		1,"zhangsan",'m',26,"handan",
+	}
+	fmt.Printf("The stuOne is %#v\n ",stuOne)
+
+	// 指定成员初始化，没有初始化的成员，自动赋值
+	sTwo := Student{addr:"hengshui"}
+
+	fmt.Printf("The stuOne is %#v\n ",sTwo)
+}
