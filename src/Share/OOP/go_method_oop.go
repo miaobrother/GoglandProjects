87400fb577bdd0b3a217903d69d2b776e396dc11
miaobrother <zihao121908@gmail.com>
2018-04-17 21:41:50 +0800

    go中方法的继承

---
+++ src/Share/OOP/go_method_oop.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+type person struct {
+	name string
+	sex byte
+}
+
+func (tmp *person) PrintfInfo()  {//这是一个方法
+	fmt.Printf("The name is :%s sex is :%c\n",tmp.name,tmp.sex)
+}
+
+type student struct {
+	person //匿名字段
+	id int
+}
+
+func main() {
+	s := student{person{"zhangsan",'f'},20}
+	s.PrintfInfo() //The name is :zhangsan sex is :f 可以看到student继承了person的类 实现了PrintInfo方法的调用
+}
