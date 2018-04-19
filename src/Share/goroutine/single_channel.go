8c64282a5b9ff5816692a3f3432a4ade860ea34e
miaobrother <zihao121908@gmail.com>
2018-04-19 17:41:04 +0800

    使用go的单向读写计算数值

---
+++ src/Share/goroutine/single_channel.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	//"database/sql"
+	"fmt"
+)
+
+func producer(out chan <- int)  {//只写channel
+	for i := 0;i < 10;i ++{
+		out <- i*i
+	}
+	close(out)
+
+}
+
+func consumer(in <- chan int)  {//只读channel
+	for num := range in {
+		fmt.Println("num = ",num)
+	}
+}
+
+func main() {
+	ch := make(chan int)
+
+	go producer(ch)
+
+	consumer(ch)//主协程 先执行
+}
