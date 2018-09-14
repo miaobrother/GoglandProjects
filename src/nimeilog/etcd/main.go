8ede148975499f5b128c4cf8ea3c7202aa5711dc
miaobrother <zihao121908@gmail.com>
2018-09-14 10:00:29 +0800

    main add put get method

--- src/nimeilog/etcd/main.go
+++ src/nimeilog/etcd/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	etcd_client "github.com/coreos/etcd/clientv3"
 	"time"
+	"golang.org/x/net/context"
 )
 
 func main(){
@@ -11,12 +12,33 @@ func main(){
 		Endpoints:[]string{"localhost:2379"},
 		DialTimeout: 1 * time.Second,
 	})
+	defer cli.Close()
 	if err != nil{
 		fmt.Println("connect failed,error is:",err)
 		return
 	}
-	fmt.Println("connect success")
-	defer cli.Close()
+	//fmt.Println("connect success")
+
+	ctx,cancel := context.WithTimeout(context.Background(),1*time.Second)
+	_,err = cli.Put(ctx,"../conf/logconf.conf","simple_value")
+	cancel()
+	if err != nil{
+		fmt.Println("put ctx simple_value failed.")
+		return
+	}
+	fmt.Println("Put ctx simple_value success.")
+
+	ctx,cancel = context.WithTimeout(context.Background(),time.Second)
+	resp,err := cli.Get(ctx,"../conf/logconf.conf")
+	cancel()
+	if err != nil{
+		fmt.Println("Get failed,err:",err)
+		return
+	}
+	for _, ev := range resp.Kvs{
+		fmt.Printf("key is:%v valuse is:%v\n",string(ev.Key),string(ev.Value))
+	}
+
 }
 
 
