c4ba9ca8d1f34bd942a2100eda0920fbe10642ab
miaobrother <zihao121908@gmail.com>
2018-09-16 16:48:48 +0800

    struct required filed

--- src/gin_example/main.go
+++ src/gin_example/main.go
@@ -1,27 +1,32 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-	"github.com/astaxie/beego/logs"
-	"net/http"
 	"fmt"
+	"github.com/astaxie/beego/logs"
+	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
-func logout(c *gin.Context)  {
-	c.JSON(http.StatusOK,gin.H{
-		"message":"logout success",
+type Login struct {
+	User     string `from:"user" json:"user" binding:"required"`
+	Password string `from:"password" json:"password" binding:"required"`
+}
+
+func logout(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "logout success",
 	})
 }
 
-func submit(c *gin.Context)  {
-	c.JSON(http.StatusOK,gin.H{
-		"message":"submit success",
+func submit(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "submit success",
 	})
 }
 
-func info(c *gin.Context)  {
-	c.JSON(http.StatusOK,gin.H{
-		"message":"info success",
+func info(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "info success",
 	})
 }
 func main() {
@@ -29,48 +34,61 @@ func main() {
 	r.GET("/user/search/:username/:address", func(c *gin.Context) {
 		username := c.Param("username")
 		address := c.Param("address")
-		c.JSON(200,gin.H{
-			"message" :"pong",
-			"username" :username,
-			"address" : address,
+		c.JSON(200, gin.H{
+			"message":  "pong",
+			"username": username,
+			"address":  address,
 		})
 	})
 	r.POST("/upload", func(c *gin.Context) {
-		file,err := c.FormFile("file")
-		if err != nil{
-			c.JSON(http.StatusInternalServerError,gin.H{
-				"message":err.Error(),
+		file, err := c.FormFile("file")
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"message": err.Error(),
 			})
 			return
 		}
-		logs.Debug("upload %v failed.error is:%v\n",file,err)
-		dst := fmt.Sprintf("/tmp/%v",file.Filename)
-		c.SaveUploadedFile(file,dst)
-		c.JSON(http.StatusOK,gin.H{
-			"message":fmt.Sprintf("file %v upload succes\n",file.Filename),
+		logs.Debug("upload %v failed.error is:%v\n", file, err)
+		dst := fmt.Sprintf("/tmp/%v", file.Filename)
+		c.SaveUploadedFile(file, dst)
+		c.JSON(http.StatusOK, gin.H{
+			"message": fmt.Sprintf("file %v upload succes\n", file.Filename),
 		})
 	})
 
 	r.POST("/multiupload", func(c *gin.Context) {
-		from ,_ := c.MultipartForm()
+		from, _ := c.MultipartForm()
 		files := from.File["file"]
-		for _,value := range files{
+		for _, value := range files {
 			fmt.Println(value.Filename)
-			dst := fmt.Sprintf("/tmp/%s_%d",value.Filename,value.Size)
-			c.SaveUploadedFile(value,dst)
+			dst := fmt.Sprintf("/tmp/%s_%d", value.Filename, value.Size)
+			c.SaveUploadedFile(value, dst)
 		}
-		c.JSON(http.StatusOK,gin.H{
-			"message":fmt.Sprintf("%d file upload",len(files)),
+		c.JSON(http.StatusOK, gin.H{
+			"message": fmt.Sprintf("%d file upload", len(files)),
 		})
 
 	})
 
-
 	v1 := r.Group("/v1")
 	{
-		v1.POST("/logout",logout)
-		v1.POST("/submit",submit)
-		v1.POST("/user/info",info)
+		v1.POST("/logout", logout)
+		v1.POST("/submit", submit)
+		v1.POST("/user/info", info)
 	}
+
+	r.POST("/loginJSON", func(c *gin.Context) {
+		var login Login
+		if err := c.ShouldBindJSON(&login); err == nil {
+			c.JSON(http.StatusOK, gin.H{
+				"user":     login.User,
+				"password": login.Password,
+			})
+		} else {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"err": err.Error(),
+			})
+		}
+	})
 	r.Run(":9090")
 }
