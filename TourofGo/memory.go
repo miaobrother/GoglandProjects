3c33495d2e82cabc258b604f75a910ffe8df43c3
miaobrother <zihao121908@gmail.com>
2018-12-07 18:29:27 +0800

    ve2x GC go code

---
+++ TourofGo/memory.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"strconv"
+	"time"
+)
+
+func do() {
+	max := 2100000
+	item := 15111
+	var a []int
+	for range make([]struct{},max){
+		a = append(a,item)
+	}
+	var sitem []string
+	for range make([]struct{},max){
+		sitem = append(sitem,strconv.Itoa(item))
+	}
+	fmt.Println(len(sitem))
+	runtime.GC()
+}
+
+func main() {
+	for range make([]struct{},10){
+		go do()
+	}
+	time.Sleep(time.Second*30)
+}
