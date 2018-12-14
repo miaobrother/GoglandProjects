adf9171205ee2b426b1cb002a5e2d8932bea2146
miaobrother <zihao121908@gmail.com>
2018-12-14 15:27:21 +0800

    switch true 用法

---
+++ TourofGo/switch-with-no-condition.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	t := time.Now()
+	fmt.Println("t.hour:",t.Hour())
+	fmt.Printf("The t is %v\n",t)
+	switch  {
+	case t.Hour() < 12:
+		fmt.Println("Good morning!")
+	case t.Hour() < 17:
+		fmt.Println("Good afternoon!")
+	default:
+		fmt.Println("Good evening.")
+	}
+}
