9dfeb4921cab9422a37d7b79bdc41e63ae0b5e0a
miaobrother <zihao121908@gmail.com>
2019-01-05 21:26:11 +0800

    实现sortbool

---
+++ TourofGo/Advance/init_array.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+
+func main (){
+	names :=  [3]string{2:"zhangsan",0:"lisi",}
+	fmt.Printf("The name is:%v\n",names)
+}
---
+++ TourofGo/Advance/rand.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func randValue() [10]int {
+	rand.Seed(time.Now().Unix())
+	var dict [10]int
+	for i := 0; i < len(dict); i++ {
+		dict[i] = rand.Intn(200)
+	}
+	return dict
+}
+//数组做函数参数，默认是值传递
+func sortBool(dict [10]int) [10]int {
+	for i := 0; i < len(dict); i++ {
+		for j := 0; j < len(dict)-i-1; j++ {
+			if dict[j] > dict[j+1] {
+				dict[j], dict[j+1] = dict[j+1], dict[j]
+			}
+		}
+	}
+	return dict
+}
+
+func main() {
+	//randValue()
+	fmt.Printf("The sortedbool is:%v\n",sortBool(randValue()))
+
+}
